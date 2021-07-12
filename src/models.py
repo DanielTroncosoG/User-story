5e2c66b68051da5b000a217be924984f5a9e4575
DanielTroncosoG <di.trgonzalez@gmail.com>
2021-07-12 20:39:28 +0000

    commit usuarios

--- src/models.py
+++ src/models.py
@@ -8,27 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+class Biker(Base):
+    __tablename__ = "biker"
+    id = Column(Integer, primary_key = True)
+    bikername = Column(String, unique = True)
+    firstname = Column(String)
+    lastname = Column(String) 
+    email = Column(String,unique=True)
 
-    def to_dict(self):
-        return {}
+class Helper(Base):
+    __tablename__ = "helper"
+    id = Column(Integer, primary_key = True)
+    author_id = Column(Integer, ForeignKey("biker.id"))
+    biker = relationship(Biker)    
 
+class Blog(Base):
+    __tablename__ = "Blog"
+    id = Column(Integer, primary_key = True)
+    comment_text = Column(String)
+    author_id = Column(Integer, ForeignKey("biker.id"))
+    biker = relationship(Biker)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    post = relationship(Post)
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key = True)
+    biker_id = Column(Integer, ForeignKey("biker.id"))
+    biker = relationship(Biker)
+
+class Comment(Base):
+    __tablename__ = "comment"
+    id = Column(Integer, primary_key = True)
+    comment_text = Column(String)
+    author_id = Column(Integer, ForeignKey("biker.id"))
+    user = relationship(User)
+    post_id = Column(Integer, ForeignKey("post.id"))
+    post = relationship(Post)
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
